d16c13328c8fb1e7d7139b9f399d95dee9e5ae02
Jeffrey Martin <jeffreymartin93@gmail.com>
2022-01-27 14:13:22 -0500

    improved user input

--- pierrot_chain_generator.py
+++ pierrot_chain_generator.py
@@ -10,8 +10,8 @@ This file contains functionality for generating chains.
 from pctheory import pcseg, pitch
 
 pc = pitch.PitchClass(9)
-chains = pcseg.make_chain_weak(pc, ["[015]", "[014]", "[026]", "[037]"], 0.4, 0.4)
-chains = pcseg.make_chain_weak(pc, ["[0148]", "[0146]", "[0137]"], 0.4, 0.2)
+chains = pcseg.generate_chains_weak(pc, ["[015]", "[014]", "[026]", "[037]"], 0.4, 0.4)
+chains = pcseg.generate_chains_weak(pc, ["[0148]", "[0146]", "[0137]"], 0.4, 0.2)
 
 chains2 = []
 [chains2.append(c) for c in chains if c not in chains2]
