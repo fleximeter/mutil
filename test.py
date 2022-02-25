e9c83ab07cfa4a186346ebe06afd4e50c95ab03b
fleximeter <59806547+fleximeter@users.noreply.github.com>
2022-02-25 12:18:41 -0500

    Add files via upload

--- test.py
+++ test.py
@@ -1,10 +1,3 @@
-from pctheory import group, pcseg, pcset, pitch, set_complex, tables, transformations, util
+from pctheory import array, group, pcseg, pcset, pitch, set_complex, tables, transformations, util
 
-s4 = pcseg.make_pcseg(0, 11, 7, 8, 3, 1, 2, 10, 6, 5, 4, 9)
-s4mx = pcseg.TwelveToneMatrix(s4)
-pc = [pitch.PitchClass(n) for n in range(12)]
-tmx = pcseg.InvarianceMatrix("T", s4, s4)
-imx = pcseg.InvarianceMatrix("I", s4, s4)
-
-imx.print([pc[11]])
 
