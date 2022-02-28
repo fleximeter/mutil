bc473db0a9f52a7557242d3a5e9b0db9db9c2b14
fleximeter <59806547+fleximeter@users.noreply.github.com>
2022-02-28 10:49:48 -0500

    arrays

--- pierrot_arrays.py
+++ pierrot_arrays.py
@@ -303,7 +303,7 @@ arrays = [
     ]
 ]
 
-t = array.transform_row_content(arrays[2][0], transformations.RO(3, 0, 1))
-m = array.make_array_chain(t, 2, False)
+t = array.transform_row_content(arrays[4][1], transformations.RO(5, 0, 11))
+m = array.make_array_chain(t, 1, False)
 for i in range(len(m)):
     print(m[i])
