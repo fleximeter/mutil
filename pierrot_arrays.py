f58576323d042a1c565f26a04b6871776549ca30
Jeffrey Martin <jeffreymartin93@gmail.com>
2022-04-11 19:30:32 -0400

    pierrot

--- pierrot_arrays.py
+++ pierrot_arrays.py
@@ -303,7 +303,7 @@ arrays = [
     ]
 ]
 
-t = array.transform_row_content(arrays[3][0], transformations.RO(9, 0, 11))
+t = array.transform_row_content(arrays[2][0], transformations.RO(11, 0, 11))
 m = array.make_array_chain(t, 1, False)
 for i in range(len(m)):
     print(m[i])
