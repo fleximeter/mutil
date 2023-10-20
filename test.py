61bfb162aa3c245bd040772bbbeacc4c2aeabdfe
Jeff Martin <jeffreymartin@outlook.com>
2023-10-19 19:36:19 -0500

    ...

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 from pctheory import pcseg, pcset, pseg, pset, pitch, transformations
 
 ros = transformations.get_otos12()
-otos24 = transformations.get_otos24()
 
 x = pcseg.generate_random_all_trichord_row()
 print(x)
-print(otos24["T5I"].transform(x))
+print(pcseg.imb_n(x, 3))
+print(ros["T5I"].transform(x))
