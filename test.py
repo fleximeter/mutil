db998d59094e68bbf1d4d0fab75520d6d3207375
Jeff Martin <jeffreymartin@outlook.com>
2024-03-01 17:28:23 -0600

    added trombone separate file

--- test.py
+++ test.py
@@ -1,17 +1,3 @@
-from pctheory.pcseg import make_pcseg12, transform, transform_hstack
+from pctheory import pitch, pcset, pcseg
 
-pcseg1 = make_pcseg12(0, 3, 5, 1, 5, 0, 3)
-pcseg2 = make_pcseg12(0, 1, 5, 3, 0, 3, 5)
-
-transf = [
-    "T5Ir5",
-    "T7Ir5",
-]
-
-for t in transf:
-    print(t, transform(pcseg1, t))
-
-for t in transf:
-    print(t, transform(pcseg2, t))
-
-print(transform_hstack(pcseg1, transf))
+x = pcseg.make_pcseg12(0, 5, 1, 2)
