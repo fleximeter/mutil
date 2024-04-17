800f010fdaea9728dfd5b7cfddf9af8e2aa9c157
Jeff Martin <jeffreymartin@outlook.com>
2024-04-17 15:42:21 -0500

    updated the chin analyzer

--- analyze/chin.py
+++ analyze/chin.py
@@ -38,7 +38,7 @@ def c_analyze():
     # path = path_laptop
     xml = f"{path}chin_etude_1_6staff.musicxml"
     output = f"{path}analysis\\entire_piece.xlsx"
-    output_general = f"{path}analysis\\statistics.csv"
+    output_general = f"{path}analysis\\statistics.xlsx"
     results_path = f"{path}analysis\\data.json"
     
     # Record starting time
@@ -54,20 +54,20 @@ def c_analyze():
     salami_slice_analyze.write_general_report("Full piece", output_general, "w", results[0], results[0].lower_bound,
                                    results[0].upper_bound)
     salami_slice_analyze.write_report(output, results[0])
-    salami_slice_analyze.write_statistics(f"{path}\\analysis\\csegs.csv", "Cseg,Frequency,Duration\n",
-                               [results[0].cseg_frequency, results[0].cseg_duration])
-    salami_slice_analyze.write_statistics(f"{path}\\analysis\\psets.csv", "Pset,Frequency,Duration\n",
+    salami_slice_analyze.write_statistics(f"{path}\\analysis\\chord_spacing_contours.xlsx", ["chord_spacing_contour", "frequency", "duration"],
+                               [results[0].chord_spacing_contour_frequency, results[0].chord_spacing_contour_duration])
+    salami_slice_analyze.write_statistics(f"{path}\\analysis\\psets.xlsx", ["pset", "frequency", "duration"],
                                [results[0].pset_frequency, results[0].pset_duration])
-    salami_slice_analyze.write_statistics(f"{path}\\analysis\\pscs.csv", "PSC,Frequency,Duration\n",
+    salami_slice_analyze.write_statistics(f"{path}\\analysis\\pscs.xlsx", ["psc", "frequency", "duration"],
                                [results[0].psc_frequency, results[0].psc_duration])
-    salami_slice_analyze.write_statistics(f"{path}\\analysis\\pcscs.csv", "SC,Frequency,Duration\n",
+    salami_slice_analyze.write_statistics(f"{path}\\analysis\\pcscs.xlsx", ["sc", "frequency", "duration"],
                                [results[0].pcsc_frequency, results[0].pcsc_duration])
 
     make_charts_general(results[0], f"{path}analysis\\graphs")
     
     for i, result in enumerate(results_staff):
         salami_slice_analyze.write_report(f"{path}analysis\\entire_piece_staff{i+1}.xlsx", result[0])
-        salami_slice_analyze.write_statistics(f"{path}\\analysis\\psets_staff{i+1}.csv", "Pset,Frequency,Duration\n",
+        salami_slice_analyze.write_statistics(f"{path}\\analysis\\psets_staff{i+1}.xlsx", ["pset", "frequency", "duration"],
                                 [result[0].pset_frequency, result[0].pset_duration])
         make_charts_specific(result[0], f"{path}analysis\\graphs_staff{i+1}")
     
@@ -87,16 +87,16 @@ def make_charts_general(results, path):
     :param path: The file path
     :return:
     """
-    chart.chart_cardinality(results, False, "Pset Cardinality Graph for Unsuk Chin’s \"In C\"",
+    chart.chart_cardinality(results, False, "Chord Cardinality Graph for Unsuk Chin’s \"In C\"",
                             size=(6.5, 3), path=f"{path}\\card_m")
-    chart.chart_cardinality(results, True, "Pset Cardinality Graph for Unsuk Chin’s \"In C\"",
+    chart.chart_cardinality(results, True, "Chord Cardinality Graph for Unsuk Chin’s \"In C\"",
                             size=(6.5, 3), path=f"{path}\\card_t")
     chart.chart_pitch_onset(results, False, "Pitch Onsets in Unsuk Chin’s \"In C\"", (6.5, 3),
                             f"{path}\\onset_measure")
-    chart.chart_pset_spacing_index(results, False, "Pset Spacing Indices in Unsuk Chin’s \"In C\"",
-                                   (6.5, 3), f"{path}\\pset_spacing_index_m")
-    chart.chart_pset_spacing_index(results, True, "Pset Spacing Indices in Unsuk Chin’s \"In C\"",
-                                   (6.5, 3), f"{path}\\pset_spacing_index_t")
+    chart.chart_chord_spacing_index(results, False, "Chord Spacing Indices in Unsuk Chin’s \"In C\"",
+                                   (6.5, 3), f"{path}\\chord_spacing_index_m")
+    chart.chart_chord_spacing_index(results, True, "Chord Spacing Indices in Unsuk Chin’s \"In C\"",
+                                   (6.5, 3), f"{path}\\chord_spacing_index_t")
     chart.chart_pitch_onset(results, True, "Pitch Onsets in Unsuk Chin’s \"In C\"", (6.5, 3),
                             f"{path}\\onset_time")
     chart.chart_pitch_duration(results, "Pitch Duration in Unsuk Chin’s \"In C\"", (6.5, 3),
