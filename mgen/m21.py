ebcc68301e08f523033931e267b373bf85f52687
Jeff Martin <jeffreymartin@outlook.com>
2024-04-28 12:25:29 -0500

    moved tempo over from pctheory

--- mgen/m21.py
+++ mgen/m21.py
@@ -10,6 +10,7 @@ This file contains functions for working with music21 and pctheory.
 import music21
 import pctheory.pitch
 
+
 def m21_make_pseg(item):
     """
     Makes a pseg from a music21 object
@@ -18,12 +19,32 @@ def m21_make_pseg(item):
     """
     pseg2 = []
     if type(item) == music21.note.Note:
-        pseg2.append(pctheory.pitch.Pitch12(item.pitch.midi - 60))
+        pseg2.append(pctheory.pitch.Pitch(item.pitch.midi - 60))
     elif type(item) == music21.pitch.Pitch:
-        pseg2.append(pctheory.pitch.Pitch12(item.pitch.midi - 60))
+        pseg2.append(pctheory.pitch.Pitch(item.pitch.midi - 60))
     elif type(item) == music21.chord.Chord:
         for p in item.pitches:
-            pseg2.append(pctheory.pitch.Pitch12(p.midi - 60))
+            pseg2.append(pctheory.pitch.Pitch(p.midi - 60))
     else:
         raise TypeError("Unsupported music21 type")
     return pseg2
+
+
+def m21_make_pset(item) -> set:
+    """
+    Makes a pset from a music21 object
+    :param item: A music21 object
+    :return: A pset
+    *Compatible only with chromatic psegs
+    """
+    pset2 = set()
+    if type(item) == music21.note.Note:
+        pset2.add(pctheory.pitch.Pitch(item.pitch.midi - 60, 12))
+    elif type(item) == music21.pitch.Pitch:
+        pset2.add(pctheory.pitch.Pitch(item.pitch.midi - 60, 12))
+    elif type(item) == music21.chord.Chord:
+        for p in item.pitches:
+            pset2.add(pctheory.pitch.Pitch(p.midi - 60, 12))
+    else:
+        raise TypeError("Unsupported music21 type")
+    return pset2
