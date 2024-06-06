3393f26520e867c458e2ede1452a00455c95ca32
Jeff Martin <jeffreymartin@outlook.com>
2024-06-06 14:09:58 -0500

    first version of midi working

--- test.py
+++ test.py
@@ -1,7 +1,26 @@
-import mgen.tempo as tempo
-from fractions import Fraction
+import mido
 
-tempi = (48, 60, 72)
-durations = (4, 2, Fraction(2, 3), 1, Fraction(1, 2), Fraction(2, 5), Fraction(1, 3), Fraction(1, 4), Fraction(1, 5), Fraction(1, 6), Fraction(1, 7), Fraction(1, 8), Fraction(1, 9))
+QTR = 480
+TEMPO_MULTIPLIER = 1000000
+TEMPO = 60
 
-tempo.plot_tempo_table(tempi, durations)
+notes = [60, 62, 64, 65, 67, 69, 71, 72] * 4 * 4
+
+meta_track = mido.MidiTrack()
+meta_track.append(mido.MetaMessage('time_signature', numerator=4, denominator=4, clocks_per_click=24, notated_32nd_notes_per_beat=8, time=0))
+meta_track.append(mido.MetaMessage('set_tempo', tempo=TEMPO * TEMPO_MULTIPLIER // 60, time=0))
+meta_track.append(mido.MetaMessage('key_signature', key='C', time=0))
+meta_track.append(mido.MetaMessage('end_of_track', time=0))
+track1 = mido.MidiTrack()
+track1.append(mido.MetaMessage('track_name', name="Track1", time=0))
+
+for note in notes:
+    track1.append(mido.Message('note_on', channel=0, note=note, velocity=64, time=0))
+    track1.append(mido.Message('note_off', channel=0, note=note, velocity=0, time=QTR//8))
+
+track1.append(mido.MetaMessage('end_of_track', time=0))
+
+mid = mido.MidiFile()
+mid.tracks.append(meta_track)
+mid.tracks.append(track1)
+mid.save("data/song.mid")
