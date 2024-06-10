171ffb953c7a1030064f4061d0aec81465ac5a6a
Jeff Martin <jeffreymartin@outlook.com>
2024-06-10 15:51:11 -0500

    small mod

--- midi_gen.py
+++ midi_gen.py
@@ -24,17 +24,17 @@ QTR = 480
 
 
 # A virtuosic high line
-dur = QTR // 2
-num_notes = 200
-path = "data\\song4.mid"
-z = algorithms.NoteEnvelope([(55, 65), (55, 72), (60, 72), (60, 77), (60, 72)], 
-                            [0, int(num_notes*0.3), int(num_notes*0.5), int(num_notes*0.7), int(num_notes)])
-notes = algorithms.wander_nth_int(57, num_notes, 
-                        [1, 2, 4, 5, 7, 11], 
-                        [(2, 2), (9, 6), (7, 3), (7, 3), (5, 2), (4, 1)],
+dur = QTR // 4
+num_notes = 2000
+path = "data\\song3.mid"
+z = algorithms.NoteEnvelope([(60, 77), (60, 84), (65, 96), (72, 96), (72, 84), (60, 84), (60, 96), (60, 79)], 
+                            [0, int(num_notes*0.15), int(num_notes*0.3), int(num_notes*0.45), int(num_notes*0.55), int(num_notes*0.7), int(num_notes*0.85), int(num_notes)])
+notes = algorithms.wander_nth_int(65, num_notes, 
+                        [0, 2, 4, 5, 7, 11, 14, 17], 
+                        [(7, 7), (7, 5), (7, 5), (4, 3), (5, 3), (4, 2), (4, 2), (2, 1)],
                         z,
                         5,
-                        4)
+                        6)
 algorithms.stochastic_add_rests(notes, 15, 10)
 durations = algorithms.rubato(notes, dur, dur * 0.02)
 algorithms.make_midi_file(path, notes, durations, "4/4")
