f6df09f4f2da604abf2f3bb9653728536a83f580
Jeff Martin <jeffreymartin@outlook.com>
2025-04-12 19:33:35 -0500

    more work on chamber piece

--- chamber_piece_linegen2.py
+++ chamber_piece_linegen2.py
@@ -3,14 +3,16 @@
 from mgen import xml_gen
 from pctheory import pitch
 from mgen.comptools import multiply, rotate
+import music21
 
 score = xml_gen.create_score("Line Generator", "Jeff Martin")
-xml_gen.add_instrument(score, "Violin", "Vln.")
+xml_gen.add_instrument(score, "Cello", "Vc.")
 xml_gen.add_measures(score, 50, 1, None, "4/4")
+xml_gen.add_item(score[1], music21.clef.BassClef(), 1)
 
 # the 4 forms of the basic sequence of pitch intervals
 seq = {
-    'p': [-2, 1, 2, -3, -2, 1, 3, -5, 3]
+    'p': [-2, 1, -4, 1, 5, -1, 2, -5]
 }
 seq['r'] = seq['p'].copy()
 seq['r'].reverse()
@@ -19,10 +21,9 @@ seq['ri'] = seq['i'].copy()
 seq['ri'].reverse()
 
 int_seq = seq['p'] + seq['p'] + seq['i'] + rotate(seq['p'], 2) + seq['p'] + seq['r']
-# int_seq = seq['i'] + seq['p'] + seq['ri'] + rotate(seq['p'], 2) + seq['p'] + seq['r']
 
 # build the sequence with a starting pitch
-seq_builder = [pitch.Pitch(88)]
+seq_builder = [pitch.Pitch(45)]
 
 # the sequence length
 LENGTH = 100
@@ -34,5 +35,5 @@ rhythm = [1 for i in range(len(seq_builder))]
 mseq = xml_gen.make_music21_list(seq_builder, rhythm)
 xml_gen.add_sequence(score[1], mseq)
 xml_gen.remove_empty_measures(score)
-xml_gen.export_to_xml(score, "data/score1.musicxml")
+xml_gen.export_to_xml(score, "data/score2.musicxml")
 score.show()
